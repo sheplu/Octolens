97a7d5e59ed0920c0d5896b852c4d8c5b1e3cfb2
Jean Burellier <jean.burellier@gmail.com>
2025-08-16 22:01:12 +0200

    feat: fetch codeowners repository information

--- src/repository.js
+++ src/repository.js
@@ -1,16 +1,18 @@
-import { getDependabot, getRepositoryTeams } from "@sheplu/yagi";
+import { getCodeowners, getDependabot, getRepositoryTeams } from "@sheplu/yagi";
 
 export async function computeRepository(owner, repository) {
 	let repositoryPromises = [];
 	repositoryPromises.push(
 		getRepositoryTeams(owner, repository),
-		getDependabot(owner, repository)
+		getDependabot(owner, repository),
+		getCodeowners(owner, repository)
 	);
 
 	const data = await Promise.all(repositoryPromises);
 
 	return {
 		teams: data[0],
-		dependabot: data[1]
+		dependabot: data[1],
+		codeowners: data[2],
 	}
 };
