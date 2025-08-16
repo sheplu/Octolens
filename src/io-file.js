239f3c20e1b7677bac3b13a095e52fc001977a3d
Jean Burellier <jean.burellier@gmail.com>
2025-08-16 10:19:40 +0200

    feat: fetch all repositories from an organisation

---
+++ src/io-file.js
@@ -0,0 +1,9 @@
+import { writeFileSync } from "node:fs";
+
+export function writeFile(path, data) {
+	try {
+		writeFileSync(path, data)
+	} catch (error) {
+		console.error(error)
+	}
+};
---
+++ src/organisation.js
@@ -0,0 +1,39 @@
+import { listRepositories } from "@sheplu/yagi";
+import { writeFile } from "./io-file.js";
+
+export async function getRepositoriesFromOrganisation(organisation, path='repositories.json') {
+	const repositories = await listRepositories(organisation);
+	const cleanedRepositories = [];
+	for (const repository of repositories) {
+		cleanedRepositories.push(cleanRepositoryObject(repository));
+	}
+	writeFile(path, JSON.stringify(cleanedRepositories));
+	return cleanedRepositories;
+};
+
+function cleanRepositoryObject(repository) {
+	return {
+		id: repository.id,
+		name: repository.name,
+		full_name: repository.full_name,
+		private: repository.private,
+		description: repository.description,
+		fork: repository.fork,
+		created_at: repository.created_at,
+		updated_at: repository.updated_at,
+		pushed_at: repository.pushed_at,
+		size: repository.size,
+		language: repository.language,
+		has_issue: repository.has_issue,
+		has_wiki: repository.has_wiki,
+		archived: repository.archived,
+		disabled: repository.disabled,
+		license: repository.license,
+		web_commit_signoff_required: repository.web_commit_signoff_required,
+		topics: repository.topics,
+		visibility: repository.visibility,
+		default_branch: repository.default_branch,
+		permissions: repository.permissions,
+		security_and_analysis: repository.security_and_analysis
+	};
+};
