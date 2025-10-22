7f5351708bbc6562824f7bea2d20e515a4d1a3c8
Jean Burellier <jean.burellier@gmail.com>
2025-10-22 09:59:27 +0200

    feat: add default check with ajv

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-import { scanRepository } from './src/scan-repository.js';
+import { assertCompliance, scanRepository } from './src/scan-repository.js';
 import { writeFile } from './src/io-file.js';
 
 async function main() {
-	const repo = await scanRepository('owner', 'repository');
+	const repo = await scanRepository('owner', 'repository', true);
 
 	writeFile('repository.json', JSON.stringify(repo));
+	const compliance = assertCompliance(repo);
+
+	writeFile('compliance.json', JSON.stringify(compliance));
 };
 
 main();
